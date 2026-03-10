cc3a716425e73f20657a0828ada37964d9543d51
Gabriel Kras <gabriel.kras@hotmail.com>
2026-03-09 22:04:57 -0300

    iniciando a inclusao de comentarios

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,62 @@ import { Post } from './components/Post'
 
 import styles from './App.module.css'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/cthaiur.png',
+      name: 'Felipe Silveira',
+      role: 'Fullstack Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      { 
+        type: 'paragraph', 
+        content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que desenvolvi na NLW Return, evento da Rocketseat. Nele, implementei diversas funcionalidades, como autenticação, banco de dados e interface responsiva.' 
+      },
+      { type: 'link', content: '👉 [link](https://github.com/thaiur/nlw-return)' },
+      { type: 'link', content: '#novoprojeto' },
+      { type: 'link', content: '#nlw' },
+      { type: 'link', content: '#rocketseat' },
+    ],
+    publishedAt: new Date('2026-02-10 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://i.pravatar.cc/600',
+      name: 'Marcela Braga',
+      role: 'Web Developer Top'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala rapeize 👋' },
+      { 
+        type: 'paragraph', 
+        content: 'Eu não aguento mais nem olhar o VSCode, mais alguém com isso?.' 
+      },
+      { type: 'link', content: '👉 [link](https://socorro.com' },
+      { type: 'link', content: '#meajuda' },
+      { type: 'link', content: '#sos' },
+      { type: 'link', content: '#ihatejs' },
+    ],
+    publishedAt: new Date('2025-12-10 20:00:00'),
+  },
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 export function App(){
 
@@ -18,8 +74,18 @@ export function App(){
       <Sidebar />
 
         <main>
-        <Post/>
-        <Post/>
+        {posts.map(post => (
+            <Post 
+            key={post.id} 
+            author={post.author} 
+            content={post.content} 
+            publishedAt={post.publishedAt} 
+
+            />
+            ))}
+            
+              
+        
         </main>
 
       </div>
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -7,10 +7,30 @@ import { Avatar } from './Avatar'
 
 import styles from './Post.module.css'
 
+interface PostProps {
+  author: {
+    avatarUrl: string;
+    name: string;
+    role: string;
+  }
+  content: {
+    type: 'paragraph' | 'link';
+    content: string;
+  }[]
+  publishedAt: Date;
+}
 
-export function Post(){
+const comments = [
+    1, 2, 3
+]
 
-      const publishedDate = new Date('2026-03-09 14:32:22')
+
+
+
+export function Post({ author, content, publishedAt }: PostProps) {
+
+
+      const publishedDate = publishedAt
       const publishedDateFormatted = format(publishedDate, "d 'de' LLLL 'às' HH:mm", {
         locale: ptBR,
       })
@@ -25,35 +45,28 @@ export function Post(){
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <Avatar src="https://i.pravatar.cc/500" hasBorder={true}/>
+                    <Avatar src={author.avatarUrl} hasBorder={true}/>
                 
                 <div className={styles.authorInfo}>
-                <strong>Gabriel Kras</strong>
-                <span>Maluco Beleza</span>
+                <strong>{author.name}</strong>
+                <span>{author.role}</span>
                 
                 </div>
                 </div>
 
                 <time title={publishedDateFormatted} dateTime={publishedDate.toISOString()}>
-                    Publicado há {publishedDateRelativeToNow}
+                    Publicado {publishedDateRelativeToNow}
                 </time>
 
             </header>
                 <div className={styles.content}>
-                    <p>Fala galeraa 👋</p>
-
-                    <p>
-                    Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return,
-                    evento da Rocketseat. O nome do projeto é DoctorCare 🚀
-                    </p>
-
-                    <p><a href="#">👉 jane.design/doctorcare</a></p>
-
-                    <p>
-                    <a href="#">#novoprojeto</a>{' '}
-                    <a href="#">#nlw</a>{' '}
-                    <a href="#">#rocketseat</a>{' '}
-                    </p>
+                    {content.map(line => {
+                    if (line.type === 'paragraph') {
+                        return <p key={line.content}>{line.content}</p>
+                    } else if (line.type === 'link') {
+                        return <p key={line.content}><a href="#">{line.content}</a></p>
+                    }
+                    })}
                 </div>
 
                 <form className={styles.commentForm}>
@@ -67,10 +80,11 @@ export function Post(){
                 </form>
 
                 <div className={styles.commentList}>
-                    <Comment />
-                    <Comment />
-                    <Comment />
-                    
+                    {comments.map(comment => {
+                        return (
+                            <Comment key={comment} />
+                        )
+                    })}
                 </div>
 
         </article>
